b468e0b6f720d9e30af65f94ceb9a25a34e028c9
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-12-24 10:31:33 +0900

    修正

--- index2.php
+++ index2.php
@@ -1,8 +1,10 @@
 <?php
 
+// 画面を開いた際に $_GETでページIDと画面モードを取得
 $page_id = isset($_GET['page_id']) ? $_GET['page_id'] : 1;
 $mode = isset($_GET['mode']) ? $_GET['mode'] : "incomplete";
 
+// DB接続
 $url = parse_url(getenv('DATABASE_URL'));
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
@@ -15,8 +17,9 @@ switch ($mode) {
         $query = sprintf("SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = true ORDER BY id LIMIT 5 OFFSET %d;",5 * ($page_id - 1));
         break;
 }
-
+// クエリ実行
 $stmt = $pdo->query($query);
+
 ?>
 
 <html>
@@ -41,10 +44,10 @@ $stmt = $pdo->query($query);
             <?php endforeach; ?>
         </table>
         <?php if($page_id > 1): ?>
-        <a href="index2.php?<?php echo sprintf("page_id=%s&mode=%s",($page_id-1), $mode); ?>">前のページ</a>
+        <a href="index2.php?<?php echo sprintf("page_id=%s&mode=%s",($page_id - 1), $mode); ?>">前のページ</a>
         <?php else: ?>
         <span>前のページ</span>
         <?php endif; ?>
-        <a href="index2.php?<?php echo sprintf("page_id=%s&mode=%s",($page_id-2), $mode); ?>">前のページ</a>
+        <a href="index2.php?<?php echo sprintf("page_id=%s&mode=%s",($page_id + 1), $mode); ?>">次のページ</a>
     </body>
 </html>
